3e29b1b7a5b00b45a90a0d153c1013aaee4850ee
sahiba <25sahiba03@gmail.com>
2025-11-06 13:00:51 +0530

    add responsive input box

---
+++ inputRespond.js
@@ -0,0 +1,77 @@
+// const inputBox = document.querySelector("input");
+//       const button = document.querySelector("button");
+//       button.addEventListener("click", clickfun);
+//        summitValue = 1;
+
+//       function clickfun() {
+//         var listitem = document.createElement("li");
+//         listitem.innerText = inputBox.value;
+//         listitem.insertAdjacentHTML("beforeend",`<a class="deletebtn" style="float:right;"><i class="fa-solid fa-trash"></i></a> <a class="editbtn" style="float:right; margin-right: 10px;" ><i class="fa-regular fa-eye-slash"></i></a>`);
+//         document.querySelector("ol").appendChild(listitem);
+//         inputBox.value = " ";
+//       }
+// const OL = document.getElementsByTagName("ol");
+//  const inputBox = document.querySelector("input");
+//       const button = document.querySelector("button");
+//      button.addEventListener("click", clickfun);
+     
+//        summitValue = 1;
+
+//       function clickfun() {
+//         var listitem = document.createElement("li");
+//         listitem.innerText = inputBox.value;
+//         listitem.insertAdjacentHTML("beforeend",`<a class="deletebtn" style="float:right;"><i class="fa-solid fa-trash"></i></a> <a class="editbtn" style="float:right; margin-right: 10px;" ><i class="fa-regular fa-eye-slash"></i></a>`);
+//       OL.appendChild(listitem);
+//         inputBox.value = " ";
+         
+      //   var lenoficon = document.getElementsByClassName("deletebtn").length;
+      //  for(let  j= 0; j< lenoficon; j++){
+      //   document.getElementsByClassName("deletebtn")[j].addEventListener("click", removeitemfun);
+      //   function removeitemfun(){
+      //               UL.child.removeChild[j];
+      //   }
+      //  }
+
+
+        // var lenclass = document.getElementsByClassName("deletebtn").length;
+        // for (let i = 0; i < lenclass; i++) {
+
+        //    document.getElementsByClassName("deletebtn")[i];
+        //   document.getElementsByClassName("deletebtn")
+        //     [i].addEventListener("click", function () {
+        //       this.parentElement.remove();
+        //     });
+        //   document
+        //     .getElementsByClassName("editbtn")[i].addEventListener("click", function () {
+        //       inputBox.value = this.parentElement.firstChild.textContent;
+        //     });
+        // }
+      // }
+
+      //first file
+       // function renderList(items){
+      //     const ol = document.querySelector("ol");
+      //     ol.innerHTML = " ";
+      //     items.forEach(function(item, idx){
+      //         const listitem = document.createElement("li");
+      //         const span1 = document.createElement("span");
+      //         span1.innerText = item;
+      //         const btn = document.createElement("button");
+      //         const edit = document.createElement("button");
+      //         btn.innerText = "Delete";
+      //         edit.innerText = "Edit";
+      //          listitem.appendChild(edit);
+      //             edit.addEventListener("click", function(){
+      //                 const index = items.indexOf(item);
+      //                 console.log("editIndex", index, items[index]);
+      //             });
+      //         btn.addEventListener("click", function(){
+      //             const index = items.indexOf(item);
+      //             console.log(index);
+      //         });
+      //         listitem.appendChild(span1);
+      //         listitem.appendChild(btn);
+      //         ol.appendChild(listitem);
+      //     });
+      // }
+       
